1fada8af92c6238559f31fcc343057dd1a8d9361
cyhhoo <cyhhoo@gmail.com>
2026-01-13 14:45:48 +0900

    feat controller 권한 관련 수정

--- src/main/java/com/mycompany/project/config/AuditorAwareImpl.java
+++ src/main/java/com/mycompany/project/config/AuditorAwareImpl.java
@@ -1,5 +1,6 @@
 package com.mycompany.project.config;
 
+import com.mycompany.project.jwtsecurity.CustomUserDetails;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,13 +9,23 @@ import java.util.Optional;
 
 @Component
 public class AuditorAwareImpl implements AuditorAware<String> {
+
     @Override
     public Optional<String> getCurrentAuditor() {
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 인증 정보가 없거나, 익명 사용자인 경우 빈값 반환
         if (authentication == null || !authentication.isAuthenticated()
                 || authentication.getName().equals("anonymousUser")) {
             return Optional.empty(); // 로그인하지 않은 경우 (null 처리)
         }
+
+        // CustomUserDetails 인 경우 이메일 반환, 나중에 email 말고 다른 정보 필요한경우 수정 가능하도록 지금 구조로 둠
+        if (authentication.getPrincipal() instanceof CustomUserDetails){
+          return Optional.of(((CustomUserDetails) authentication.getPrincipal()).getEmail());
+        }
+
         return Optional.of(authentication.getName()); // 사용자 이메일 반환
     }
 }
