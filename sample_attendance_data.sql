d4e00492719a3b8a4504786365fa7707530837a1
chanjin346 <ckswls2231@gmail.com>
2026-01-14 11:48:57 +0900

    "api 테스트 완료"

--- sample_attendance_data.sql
+++ sample_attendance_data.sql
@@ -16,6 +16,12 @@ VALUES
   (2002, 'student2@schoolmate.local', '$2y$10$HND8tKCP8KcjTUCpcsFGae2Y7LrP2zvu43majP5r1oBhN4csqFRhu', 'Student Two', 'STUDENT', 'ACTIVE', '2008-04-04', 0, NOW(), NOW()),
   (9001, 'admin1@schoolmate.local', '$2y$10$HND8tKCP8KcjTUCpcsFGae2Y7LrP2zvu43majP5r1oBhN4csqFRhu', 'Admin One', 'ADMIN', 'ACTIVE', '1985-03-03', 0, NOW(), NOW());
 
+/* 여기 추가*/
+INSERT INTO tbl_user (user_id, email, password, name, role, status, birth_date, login_fail_count, created_at, updated_at)
+VALUES
+    (1002, 'teacher2@schoolmate.local', '$2y$10$HND8tKCP8KcjTUCpcsFGae2Y7LrP2zvu43majP5r1oBhN4csqFRhu', 'Teacher two', 'TEACHER', 'ACTIVE', '1995-02-01', 0, NOW(), NOW());
+
+
 -- Teacher detail (shared PK with user)
 INSERT INTO tbl_teacher_detail (teacher_id, subject_id, homeroom_grade, homeroom_class_no)
 VALUES (1001, 1001, 1, 1)
@@ -23,6 +29,14 @@ ON DUPLICATE KEY UPDATE
     subject_id = VALUES(subject_id),
     homeroom_grade = VALUES(homeroom_grade),
     homeroom_class_no = VALUES(homeroom_class_no);
+
+/* 여기 추가*/
+INSERT INTO tbl_teacher_detail (teacher_id, subject_id, homeroom_grade, homeroom_class_no)
+VALUES (1002, 1001, 1, 1)
+ON DUPLICATE KEY UPDATE
+                     subject_id = VALUES(subject_id),
+                     homeroom_grade = VALUES(homeroom_grade),
+                     homeroom_class_no = VALUES(homeroom_class_no);
 
 -- Student detail (shared PK with user)
 INSERT INTO tbl_student_detail (student_id, student_grade, student_class_no, student_no)
