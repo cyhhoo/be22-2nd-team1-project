51452c8d0664c37f43342b1e31add545f6b370ad
zkfmak9257 <zkfmak9257@naver.com>
2026-01-15 19:00:56 +0900

    no message

---
+++ course-service/src/main/java/com/mycompany/project/course/CourseServiceApplication 2.java
@@ -0,0 +1,30 @@
+package com.mycompany.project.course;
+
+import com.mycompany.project.common.config.GlobalFeignConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(defaultConfiguration = GlobalFeignConfig.class)
+@EnableJpaRepositories(basePackages = "com.mycompany.project")
+@EntityScan(basePackages = "com.mycompany.project")
+@MapperScan(basePackages = "com.mycompany.project", annotationClass = Mapper.class)
+@ComponentScan(basePackages = {
+        "com.mycompany.project.course",
+        "com.mycompany.project.common",
+        "com.mycompany.project.security",
+        "com.mycompany.project.exception"
+})
+public class CourseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseServiceApplication.class, args);
+    }
+}
