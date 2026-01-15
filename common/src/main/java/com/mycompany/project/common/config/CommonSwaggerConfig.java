29a3f3cf3da1a0af4d567a49955f27941e405099
cyhhoo <cyhhoo@gmail.com>
2026-01-15 17:57:41 +0900

    올림

--- common/src/main/java/com/mycompany/project/common/config/CommonSwaggerConfig.java
+++ common/src/main/java/com/mycompany/project/common/config/CommonSwaggerConfig.java
@@ -5,32 +5,43 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class CommonSwaggerConfig {
 
-    @Value("${spring.application.name:Schoolmate Service}")
-    private String applicationName;
+        @Value("${spring.application.name:Schoolmate Service}")
+        private String applicationName;
 
-    @Bean
-    public OpenAPI openAPI() {
-        String jwt = "Bearer Authentication";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
-        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-                .name(jwt)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT"));
+        @Value("${gateway.url:http://localhost:8000}")
+        private String gatewayUrl;
 
-        return new OpenAPI()
-                .components(components)
-                .info(new Info()
-                        .title(applicationName + " API")
-                        .description(applicationName + " API Documentation")
-                        .version("1.0.0"))
-                .addSecurityItem(securityRequirement);
-    }
+        @Bean
+        public OpenAPI openAPI() {
+                String jwt = "Bearer Authentication";
+                SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+                Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                                .name(jwt)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
+
+                Server gatewayServer = new Server()
+                                .url(gatewayUrl)
+                                .description("Gateway Server");
+
+                return new OpenAPI()
+                                .servers(List.of(gatewayServer))
+                                .components(components)
+                                .info(new Info()
+                                                .title(applicationName + " API")
+                                                .description(applicationName + " API Documentation")
+                                                .version("1.0.0"))
+                                .addSecurityItem(securityRequirement);
+        }
 }
